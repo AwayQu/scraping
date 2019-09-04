ff76aa2bade59c1339c8cdc601f3fc678e3d2679
AwayQu <awayquym@gmail.com>
2019-09-05 02:43:09 +0800

    build settings
    Change-Id: I11d02b146e8cc2c82713089fbbc6a6089c71a838

--- zol/build/lib/zol/spiders/wallpaper.py
+++ zol/build/lib/zol/spiders/wallpaper.py
@@ -13,4 +13,33 @@ class ZolMeinvSpider(scrapy.Spider):
     def parse(self, response):
         for detail_url in response.css("ul.pic-list2 > li.photo-list-padding > a.pic ::attr(href)").extract():
             self.log("deatail_url %s" % detail_url)
-            # yield scrapy.Request(response.urljoin(book_url), callback=self.parse_wallpaper_detail_page)
+            self.log("url join %s" % response.urljoin(detail_url))
+            yield scrapy.Request(response.urljoin(detail_url), callback=self.parse_wallpaper_detail_page)
+
+    def parse_wallpaper_detail_page(self, response):
+        self.log("parse detail")
+        for img_view_url in response.css("ul#showImg > li > a ::attr(href)").extract():
+            self.log("image view url %s" % img_view_url)
+            yield scrapy.Request(response.urljoin(img_view_url), callback=self.parse_img)
+
+    def parse_img(self, response):
+        index = 0
+        for img_url in response.css("dd#tagfbl > a ::attr(href)").extract():
+            index += 1
+            if index == 2:
+                break
+            self.log("image url %s" % img_url)
+            self.log("index %d" % index)
+            yield scrapy.Request(response.urljoin(img_url), callback=self.get_img)
+            
+    
+    def get_img(self, response):
+        
+        index = 0
+        for img_url in response.css("body > img ::attr(src)").extract():
+            index += 1
+            if index == 2:
+                break
+            self.log("real image url %s" % img_url)
+            yield {"img": img_url}
+           
