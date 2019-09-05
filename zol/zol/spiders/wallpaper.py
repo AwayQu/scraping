0c2f2dce6d95bb1f744204ed2e3375ebca6f9e08
AwayQu <awayquym@gmail.com>
2019-09-05 10:49:58 +0800

    add more wallpaper
    Change-Id: Id186910559f6df1452d1e121e0fef31000200f7e

--- zol/zol/spiders/wallpaper.py
+++ zol/zol/spiders/wallpaper.py
@@ -1,11 +1,38 @@
 import scrapy
 
 class ZolMeinvSpider(scrapy.Spider):
-    name = "changtuimeinv"
+    name = "zol"
 
     def start_requests(self):
         urls = [
             'http://desk.zol.com.cn/meinv/changtuimeinv/',
+            'http://desk.zol.com.cn/meinv/siwa/',
+            'http://desk.zol.com.cn/meinv/rihan/',
+            'http://desk.zol.com.cn/meinv/xiangchemeinv/',
+            'http://desk.zol.com.cn/meinv/dongmanmeinv/',
+            'http://desk.zol.com.cn/meinv/yangyanmeinv/',
+            'http://desk.zol.com.cn/meinv/yangyanmeinv/2.html',
+            'http://desk.zol.com.cn/meinv/yangyanmeinv/3.html',
+            'http://desk.zol.com.cn/meinv/gudianmeinv/',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/2.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/3.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/4.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/5.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/6.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/7.html',
+            'http://desk.zol.com.cn/meinv/qingchunmeinv/8.html',
+            'http://desk.zol.com.cn/meinv/mote/',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/1.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/2.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/3.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/4.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/5.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/6.html',
+            'http://desk.zol.com.cn/meinv/gaoqingmeinv/7.html',
+            'http://desk.zol.com.cn/chemo/',
+            'http://desk.zol.com.cn/chemo/2.html',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -31,10 +58,10 @@ class ZolMeinvSpider(scrapy.Spider):
             self.log("image url %s" % img_url)
             self.log("index %d" % index)
             yield scrapy.Request(response.urljoin(img_url), callback=self.get_img)
-            
-    
+
+
     def get_img(self, response):
-        
+
         index = 0
         for img_url in response.css("body > img ::attr(src)").extract():
             index += 1
@@ -42,4 +69,4 @@ class ZolMeinvSpider(scrapy.Spider):
                 break
             self.log("real image url %s" % img_url)
             yield {"img": img_url}
-           
+
